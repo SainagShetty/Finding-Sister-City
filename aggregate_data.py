2ecd3016f99506e9621a006da188608311a8ac86
sainag-shetty <sgshetty@ncsu.edu>
2018-04-10 16:20:52 -0400

    Aggregation of data - Climate

--- aggregate_data.py
+++ aggregate_data.py
@@ -6,3 +6,6 @@ proDt = pd.read_csv('data gathering/proximity/data/proximity_2.csv')
 tmp = pd.merge(cityDt, proDt, on='id')
 tmp.to_csv('city-list-with-proximity.csv',sep=',')
 
+cliDt = pd.read_csv('data gathering/climate data/data file/climate_data_complete.csv')
+tmp2 = pd.merge(tmp, cliDt, on='Geography')
+tmp2.to_csv('city-list-with-proximity-climate.csv',sep=',')
