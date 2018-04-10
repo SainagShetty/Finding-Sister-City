8c35a027a9acade032bbe525260e6f2f6cb55167
sainag-shetty <sgshetty@ncsu.edu>
2018-04-10 14:12:32 -0400

    proximity done

--- data gathering/proximity/g_maps.py
+++ data gathering/proximity/g_maps.py
@@ -1,35 +1,34 @@
-import requests
 import pandas as pd
 import math
+import requests
 
-df = pd.read_csv('proximity.csv')
-# address = "Ashford town, Alabama"
-api_key = "AIzaSyABD0S22o7uZJeHlqXTl-efJ5aa8RBg4V0"
-# api_response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address={0}&key={1}'.format(address, api_key))
-# api_response_dict = api_response.json()
+df = pd.read_csv('proximity_edited_list.csv',sep=',')
+api_key = "AIzaSyAsGdDWgWwFQDGdHxP5qpUrZS2Z46fUvAo"
 
-# if api_response_dict['status'] == 'OK':
-#     latitude = api_response_dict['results'][0]['geometry']['location']['lat']
-#     longitude = api_response_dict['results'][0]['geometry']['location']['lng']
-#     print 'Latitude:', latitude
-#     print 'Longitude:', longitude
-
-lat = []
-longi = []
-j = 0
 n = 0
+lat = []
+lng = []
 for index, row in df.iterrows():
 	if math.isnan(row.lat) or math.isnan(row.lng):
-		address = row['Geography'] + ", " + row['stateFullName']
-		api_response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address={0}&key={1}'.format(address, api_key))
-		api_response_dict = api_response.json()
-		if api_response_dict['status'] == 'OK':
-			lat.append(api_response_dict['results'][0]['geometry']['location']['lat'])
-			longi.append(api_response_dict['results'][0]['geometry']['location']['lng'])
-			row['lat'] = api_response_dict['results'][0]['geometry']['location']['lat']
-			row['lng'] = api_response_dict['results'][0]['geometry']['location']['lng']
-			n += 1
-			print n, address
-		else:
-			print "Error!"
-df.to_csv('pro.csv')
+		address = row['Geography']
+		try:
+			api_response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address={0}&key={1}'.format(address, api_key))
+			api_response_dict = api_response.json()
+			if api_response_dict['status'] == 'OK':
+				lat.append(api_response_dict['results'][0]['geometry']['location']['lat'])
+				lng.append(api_response_dict['results'][0]['geometry']['location']['lng'])
+				n += 1
+				print(n, address)
+			else:
+				lat.append('NA')
+				lng.append('NA')
+				print("Error!")
+		except:
+			lat.append('NA')
+			lng.append('NA')
+	else:
+		lat.append(row.lat)
+		lng.append(row.lng)
+df['lat'] = lat
+df['lng'] = lng
+df.to_csv('proximity_2.csv',sep=',')
--- data gathering/proximity/loc_fetch.py
+++ data gathering/proximity/loc_fetch.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 df = pd.read_csv('uscitiesv1.3.csv', sep=",")
-cityDt = pd.read_csv('../../finalcity.csv', sep=",")
+# cityDt = pd.read_csv('../../edited-list.csv', sep=",")
+cityDt = pd.read_csv('tmp_city_list.csv', sep=",")
 states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'National',
           'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Islands', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 stateDf = []
@@ -26,4 +27,4 @@ for i in states:
     cityTmp['lng'] = lng
     stateDf.append(cityTmp)
 dfLatLng = pd.concat(stateDf)
-dfLatLng.to_csv('proximity.csv', index=False)
+dfLatLng.to_csv('proximity_edited_list.csv', index=False)
