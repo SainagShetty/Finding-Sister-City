ae290cdefab41eebad9693be19791952e4fe76d9
sainag-shetty <sgshetty@ncsu.edu>
2018-04-11 14:48:42 -0400

    Aggregation of data - Election

--- aggregate_data.py
+++ aggregate_data.py
@@ -13,3 +13,7 @@ tmp2.to_csv('city-list-with-proximity-climate.csv',sep=',')
 popDt = pd.read_csv('data gathering/proximity/code/population_edited_list.csv')
 tmp2 = pd.merge(tmp, popDt, on='id')
 tmp2.to_csv('city-list-with-proximity-climate-population.csv',sep=',')
+
+eleDt = pd.read_csv('data gathering/election data/data/final_election.csv')
+tmp = pd.merge(tmp2, eleDt, on='Geography')
+tmp.to_csv('city-list-with-proximity-climate-population-election.csv',sep=',')
