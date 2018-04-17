66324c420def07ef0f70a30f55d87c5d9aafbc4d
sainag-shetty <sgshetty@ncsu.edu>
2018-04-17 12:39:33 -0400

    Aggregation of data - Economy

--- aggregate_data.py
+++ aggregate_data.py
@@ -17,3 +17,7 @@ tmp2.to_csv('city-list-with-proximity-climate-population.csv',sep=',')
 eleDt = pd.read_csv('data gathering/election data/data/final_election.csv')
 tmp = pd.merge(tmp2, eleDt, on='Geography')
 tmp.to_csv('city-list-with-proximity-climate-population-election.csv',sep=',')
+
+ecoDt = pd.read_csv('data gathering/economy/economy_data.csv')
+tmp = pd.merge(tmp2, ecoDt, on='Geography')
+tmp.to_csv('city_with_data.csv',sep=',')
